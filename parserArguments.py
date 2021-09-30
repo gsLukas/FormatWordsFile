3ccf16836aae5bad52ce3068fba31072970ddedf
zNairy <znishikkinishio@gmail.com>
2021-09-30 14:59:34 -0300

    estrutura inicial

--- parserArguments.py
+++ parserArguments.py
@@ -1,17 +1,5 @@
 from argparse import ArgumentParser
 
 def createSetupParser():
-    parser = ArgumentParser(description='Buscador de diretórios web')
-    
-    parser.add_argument('-u', '--url',  dest='url',
-                        help='Url do site alvo', required=True)
-    parser.add_argument('-w', '--wordlist', dest='wordlist',
-                        help='Caminho da wordlist', default='./assets/wordlist.txt')
-    parser.add_argument('-mc', '--match', dest='match',
-                        help='Corresponde aos códigos de status HTTP | Ex: -mc 302,404,504', default='200')
-    parser.add_argument('-s', '--ssl', help='Habilita o ssl para a requisição.',
-    default=False, action='store_true', dest='ssl')
-    parser.add_argument('-f', '--full', help='Mostra o status de todas as requisições, não apenas as bem-sucedidas.',
-    dest='full', action='store_true')
 
-    return parser, parser.parse_args()
+    #return parser, parser.parse_args()
